267542ca6185a0e78d8793b951c59757246433d1
rampatil119 <rameshwarpatil9689@gmail.com>
2025-01-28 14:46:28 +0530

    add promot

--- ai-letter/src/main.js
+++ ai-letter/src/main.js
@@ -1,31 +1,32 @@
-import GoogleGenerativeAI from"@google/generative-ai"
+
+import { generateLetter } from './counter'; 
 
 
 
 
-let fromObject = {};
+
+let formObject = {};
 const form = document.getElementById('form');
 console.log(form);
 
 const inputFiled = document.querySelectorAll('input');
 const textArea = document.querySelectorAll('textarea');
 const submit = document.getElementById('submit');
-
+const order=document.getElementById('introduction')
+const promot=document.getElementById('prompt')
+console.log(order);
 function getData() {
   inputFiled.forEach(input => {
-    fromObject[input.id] = input.value;
+    formObject[input.id] = input.value;
   });
+  formObject["prompt"]=promot.value;
 
-  textArea.forEach(area => {
-    fromObject[area.id] = area.value;
-  });
-
-  localStorage.setItem('fromObject', JSON.stringify(fromObject));
+  localStorage.setItem('formObject', JSON.stringify(formObject));
   alert('Form data saved to local storage');
 }
 
 function insert() {
-  const storeData = localStorage.getItem('fromObject');
+  const storeData = localStorage.getItem('formObject');
   if (storeData) {
     const parsedData = JSON.parse(storeData);
     Object.keys(parsedData).forEach(key => {
@@ -45,9 +46,15 @@ window.addEventListener('load', () => {
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   getData();
-  
-
-
+  generateLetter(JSON.stringify(formObject)).then((data)=>{
+    console.log(data);
+    return data;
+  })
+  .catch((error)=>{
+    console.log(error);
+  })
 
 });
+
+
 
